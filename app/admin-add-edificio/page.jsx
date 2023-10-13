3c2735f08fd8ad75528b6ca58a6ea3f0b6c29371
cristofers <valdezcristofers@gmail.com>
2023-10-13 14:50:25 -0400

    Edificio modification ready :D

--- app/admin-add-edificio/page.jsx
+++ app/admin-add-edificio/page.jsx
@@ -5,7 +5,7 @@ import Dashboard from "../../components/Dashboard";
 import Link from "next/link";
 
 import Swal from "sweetalert2";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { createClient } from "@supabase/supabase-js";
 
 const AddEdificio = () => {
@@ -13,7 +13,26 @@ const AddEdificio = () => {
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
   const supabase = createClient(supabaseUrl, supabaseKey);
+  const IDtoModify = useSearchParams().get("id");
   const router = useRouter();
+
+  useEffect(() => {
+    async function fetchData() {
+      let { data: area, error } = await supabase
+        .from("edificio")
+        .select("*")
+        .eq("id", IDtoModify);
+
+      const newInputValues = { ...inputValues };
+
+      newInputValues["id"] = area[0].id;
+      newInputValues["edificio_nombre"] = area[0].edificio_nombre;
+
+      setInputValues(newInputValues);
+    }
+
+    if (IDtoModify) fetchData();
+  }, []);
 
   useEffect(() => {
     if (sessionStorage.getItem("usuario_rol") != 3) {
@@ -27,12 +46,10 @@ const AddEdificio = () => {
     setInputValues(newInputValues);
   };
 
-  const SaveHandler = async (e) => {
-    e.preventDefault();
-    if (!ValidData()) return;
+  const SaveData = async (dataToInsert) => {
     const { data, error } = await supabase
       .from("edificio")
-      .insert([{ edificio_nombre: inputValues.edificio_nombre }])
+      .insert([dataToInsert])
       .select();
 
     if (error != null) {
@@ -54,10 +71,57 @@ const AddEdificio = () => {
           toast.addEventListener("mouseleave", Swal.resumeTimer);
         },
         titleText: "Dato Insertado",
-        text: "El dato se ha insertado correctamente.",
+        text: "El dato se ha guardado correctamente.",
         icon: "success",
         confirmButtonText: "Aceptar",
       });
+    }
+  };
+
+  const UpdateData = async (dataToInsert) => {
+    const { data, error } = await supabase
+      .from("edificio")
+      .update(dataToInsert)
+      .eq("id", IDtoModify)
+      .select();
+
+    if (error != null) {
+      Swal.fire({
+        title: "Error!",
+        text: JSON.stringify(error),
+        icon: "error",
+        confirmButtonText: "Cool",
+      });
+    } else {
+      Swal.fire({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 10000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener("mouseenter", Swal.stopTimer);
+          toast.addEventListener("mouseleave", Swal.resumeTimer);
+        },
+        titleText: "Dato Insertado",
+        text: "El dato se ha actualizado correctamente.",
+        icon: "success",
+        confirmButtonText: "Aceptar",
+      });
+    }
+  };
+
+  const SaveHandler = async (e) => {
+    e.preventDefault();
+    if (!ValidData()) return;
+
+    let data = {
+      edificio_nombre: inputValues.edificio_nombre,
+    };
+    if (IDtoModify) {
+      UpdateData(data);
+    } else {
+      SaveData(data);
     }
   };
 
@@ -74,6 +138,7 @@ const AddEdificio = () => {
           <div>
             <label htmlFor="edificio_nombre">Nombre del Edificio</label>
             <input
+              defaultValue={IDtoModify ? inputValues.edificio_nombre : ""}
               type="text"
               id="edificio_nombre"
               onChange={(e) => handleInputChange(e)}
